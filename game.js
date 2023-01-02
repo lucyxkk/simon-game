3c4bf3df70ef503b0bb48f19bc67801a07433da7
Łucja Kunikowska <lucja.kun2@gmail.com>
2023-01-02 22:34:12 +0100

    added sound to buttons

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 let buttonColors = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
 
+document.querySelector(".play").addEventListener("click", nextSequence);
+
 function nextSequence() {
     let randomNumber = (Math.floor(Math.random()*4));
     let randomChosen = buttonColors[randomNumber];
@@ -11,28 +13,37 @@ function nextSequence() {
         setTimeout(function() {
             document.querySelector("#green").classList.remove("pressed");
             
-        }, 1000);
+        }, 500);
+        let green = new Audio('sounds/green.mp3');
+                green.play();
         
     } if (randomChosen === "red") {
         document.querySelector("#red").classList.add("pressed");
         setTimeout(function() {
             document.querySelector("#red").classList.remove("pressed");
             
-        }, 1000);
+        }, 500);
+        let red = new Audio('sounds/red.mp3');
+                red.play();
     } if (randomChosen === "yellow") {
         document.querySelector("#yellow").classList.add("pressed");
         setTimeout(function() {
             document.querySelector("#yellow").classList.remove("pressed");
             
-        }, 1000);
+        }, 500);
+        let yellow = new Audio('sounds/yellow.mp3');
+                yellow.play();
     } if (randomChosen === "blue") {
         document.querySelector("#blue").classList.add("pressed");
         setTimeout(function() {
             document.querySelector("#blue").classList.remove("pressed");
             
-        }, 1000);
-    } 
-
-    
+        }, 500);
+        let blue = new Audio('sounds/blue.mp3');
+                blue.play();
+    } else {
+        new Audio('sounds/wrong.mp3');
+            wrong.play();
+    }
 }
 
