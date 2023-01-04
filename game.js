9a2193847118cafc437fcc7da980f96eb4e48c16
Łucja Kunikowska <lucja.kun2@gmail.com>
2023-01-04 22:30:31 +0100

    added function gameOver and fixed checking user input

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let buttonColors = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
+const time = 700;
 
 document.querySelector(".play").addEventListener("click", nextSequence);
 
@@ -7,20 +8,46 @@ function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosen = buttonColors[randomNumber];
   gamePattern.push(randomChosen);
+  showSequence(0);
+}
 
-  let button = document.querySelector(`#${randomChosen}`);
+function showSequence(counter) {
+  let currentColor = gamePattern[counter];
+  let button = document.querySelector(`#${currentColor}`);
   button.classList.add("pressed");
   setTimeout(function () {
     button.classList.remove("pressed");
-  }, 500);
-  let music = new Audio(`sounds/${randomChosen}.mp3`);
+  }, time);
+  let music = new Audio(`sounds/${currentColor}.mp3`);
   music.play();
+
+  counter++;
+  if (counter < gamePattern.length) {
+    setTimeout(function () {
+      showSequence(counter);
+    }, time);
+  }
 }
 
+let checkCounter = 0;
+
 function game(element) {
-  if (element.id === gamePattern[0]) {
-    nextSequence();
+  if (element.id === gamePattern[checkCounter]) {
+    checkCounter++;
+    if (checkCounter === gamePattern.length) {
+        checkCounter = 0;
+        setTimeout(nextSequence, 500);
+    }
+    
   } else {
-    document.querySelector("h1").innerHTML = "Game Over";
+    gameOver();
   }
+}
+
+function gameOver() {
+    document.querySelector("h1").innerHTML = "Game Over";
+    let music = new Audio(`sounds/wrong.mp3`);
+  music.play();
+  alert("GAME OVER, YOUR SCORE IS: " + (gamePattern.length-1));
+
 }
